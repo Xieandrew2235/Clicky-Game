5d7261f69ad8f171f20a0977c6c3d98c2810c999
Andrew <xie.andrew2235@gmail.com>
2019-04-25 16:28:59 -0700

    fixed increment to get it working, changes to nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,14 +1,26 @@
 import React from "react";
 import "./Nav.css";
 
-const Nav = props => (
-    <nav className="navbar">
-    <h1 className="brand">
-    <a href="/">{props.children}</a>
-    </h1>
-    <h1 className="">Wrong guess!</h1>
-    <h1>Score: 0 | Top Score: 0</h1>
-    </nav>
-);
+const Nav = props => 
+<nav className="nav">
+ <a href="/"> {props.title} </a>
+  <div className="nav-wrapper">
+    <a href="#" className="brand-logo right">
+      Simpsons Memory Game
+    </a>
+    <ul id="nav-mobile" className="left hide-on-med-and-down">
+      <li>
+        <a href="#">Start</a>
+      </li>
+      <li>
+        <a href="#">Reset</a>
+      </li>
+      <li>
+        <a href="#">Leaderboard</a>
+      </li>
+    </ul>
+  </div>
+</nav>;
+
 
 export default Nav;
