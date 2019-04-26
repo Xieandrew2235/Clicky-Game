ad25f84c6f4a4bb7c742f1c10c3ecb8c407164df
Andrew <xie.andrew2235@gmail.com>
2019-04-25 21:49:28 -0700

    Updates

---
+++ precache-manifest.dfb9c0ba7bb7674f0791b407e419ea05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd537d5d8912a5f41e82d176ae856e96",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "9aad3838c65569c9a53e",
+    "url": "/clickygame/static/css/main.e28784a9.chunk.css"
+  },
+  {
+    "revision": "b7e94455b40ccb98428a",
+    "url": "/clickygame/static/js/2.c9583138.chunk.js"
+  },
+  {
+    "revision": "9aad3838c65569c9a53e",
+    "url": "/clickygame/static/js/main.09b40b93.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
